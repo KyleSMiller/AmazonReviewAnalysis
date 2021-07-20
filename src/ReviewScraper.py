85983fa5611dcd335d4aa3aaaab2327746d25153
Kyle Miller <kylesmiller38@gmail.com>
2021-07-20 15:27:02 -0600

    scraper now changes pages when n is greater than the number of results listed per page

--- src/ReviewScraper.py
+++ src/ReviewScraper.py
@@ -15,11 +15,31 @@ class ReviewScraper:
         :param n:               The number of reviews to scrape. -1 for all
         """
         self.__productKeyword = productKeyword
+        self.__products = {}  # format as:
+        """
+            {"<product_id>": 
+                {
+                "name": "productName", 
+                "reviews": ["review 1", "review 2", ...]
+                }
+            }
+        """
 
+        PRODUCTS_PER_PAGE = 48  # temp hard-code of products per page
+        for page in range((n // PRODUCTS_PER_PAGE) + 1):
+            nForPage = (PRODUCTS_PER_PAGE if (n - (PRODUCTS_PER_PAGE * (page+1))) >= 0 else
+                        PRODUCTS_PER_PAGE + (n - (PRODUCTS_PER_PAGE * (page+1))))
+            self.__getProducts(nForPage, page+1)
 
+    def __openResultsPage(self, page=1):
+        """
+        Open a page of amazon search results
+        :param page:  the page number to open
+        :return:      the lxml etree of the page
+        """
         HEADERS = ({'User-Agent':
                         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit / 537.36(KHTML, like Gecko)Chrome / 44.0.2403.157 Safari / 537.36',
-                        'Accept-Language': 'en-US, en;q=0.5'
+                    'Accept-Language': 'en-US, en;q=0.5'
                     })
 
         BAD_HEADERS = {'user-agent':
@@ -33,47 +53,43 @@ class ReviewScraper:
         # I also recommend you just open up amazon yourself and load a random page between each usage of this class
         # It's not required, but anything to help your PC not look like a web-scraper is good practice
         requests.get("https://www.amazon.com/s?k=" + self.__productKeyword, headers=BAD_HEADERS)
+        print("Opening " + self.__productKeyword + " page " + str(page) + "...")
         sleep(10)
-        self.__productListPage = requests.get("https://www.amazon.com/s?k=" + self.__productKeyword,
-                                              headers=HEADERS) #proxies=proxies)
+        productListPage = requests.get("https://www.amazon.com/s?k=" + self.__productKeyword + "&page=" + str(page), headers=HEADERS)
+        productListTree = etree.ElementTree(html.fromstring(productListPage.content))
 
-        self.__productListTree = etree.ElementTree(html.fromstring(self.__productListPage.content))
-        self.__products = {}  # format as:
-        """
-            {"<product_id>": 
-                {
-                "name": "productName", 
-                "reviews": ["review 1", "review 2", ...]
-                }
-            }
-        """
-
-        title = self.__productListTree.xpath('//title')[0].text
-        if title != "Amazon.com : " + productKeyword:
+        title = productListTree.xpath('//title')[0].text
+        if title != "Amazon.com : " + self.__productKeyword:
             print("AMAZON CAUGHT THE SCRAPER -- TRY NEW HEADERS")
 
-        self.__getProducts(n)
+        return productListTree
 
-    def __getProducts(self, n):
+
+    def __getProducts(self, n, page):
         """
         Get the first n products that come up for a given keyword search
         Use the found data to set up a dict of all products
         """
         # the number of products that come up when the keyword is searched
-        title = self.__productListTree.xpath('//title')[0].text
-        showingResults = self.__productListTree.xpath('/html/body/div[1]/div[2]/span/div/span/h1/div/div[1]/div/div/span[1]')[0].text
-        productCount = int(re.sub(" results for", "", re.sub("1-[0-9]+ of ", "", showingResults)))
-        print(title)
-        print(productCount)
-        print(self.__productListPage.content)
+        productListTree = self.__openResultsPage(page)
 
-        products = self.__productListTree.xpath('//div[@data-component-type="s-search-result"]')
+        showingResults = productListTree.xpath('/html/body/div[1]/div[2]/span/div/span/h1/div/div[1]/div/div/span[1]')[0].text
+        productCount = int(re.sub(" results for", "", re.sub("[0-9]+-[0-9]+ of ", "", showingResults)))
+        print("There are " + str(productCount) + " results for the keyword: " + self.__productKeyword)
+
+        products = productListTree.xpath('//div[@data-component-type="s-search-result"]')
 
         if n == -1:
             n = int(productCount)
         for i in range(n):
             productHref = products[i].xpath('.//a[@class="a-link-normal s-no-outline"]')
-            productId = re.sub("&sr=.*", "", re.sub(".*&qid=", "", productHref[0].get("href")))
+            productId = re.sub("&.*", "", re.sub(".*&q?id=", "", productHref[0].get("href")))
+            # TODO: currently, some odd IDs are getting captured. They redirect you to other IDs.
+            # TODO: I think the [@class="a-link-normal s-no-outline"] isn't a perfect system.
+            # TODO: Figure out a fix for it, or ignore it if it's only a very small number of ID's
+            # TODO: I think it's linked to the Amazon recommended products thing
+            # print("HREF: " + str(productHref[0].get("href")))
+            print("ID: " + productId)
 
 
-test = ReviewScraper("phosphatidylserine", n=10)
+test = ReviewScraper("phosphatidylserine", 98)
